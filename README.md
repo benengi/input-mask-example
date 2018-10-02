e13415087ac767b797732c3785443b04325671cf
Бербека Анатолий <ekzo.tech@gmail.com>
2018-10-02 15:47:47 +0500

    Created input mask example.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Input mask example
+
+For input mask all regards to
+
+https://github.com/amarkes/br-masker-ionic-3
+
+
+
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BrMaskerIonic3 } from "brmasker-ionic-3";
+import {
+  FormControl,
+  FormGroup
+} from "@angular/forms";
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [BrMaskerIonic3]
 })
 export class HomePage {
+  form: FormGroup;
+  rawPhone: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private brMaskerIonic3: BrMaskerIonic3 ) {
+    this.form = this.createForm();
   }
 
+  protected createForm(): FormGroup {
+    return new FormGroup({
+      phone: new FormControl('')
+    });
+  }
+
+  getRawPhone() {
+    this.rawPhone = this.form.get('phone').value.replace(/\D/g,'');
+  }
 }
